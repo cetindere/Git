414ebc59a9c74e9e6a2f82675d4b94a477ffe1ee
cetindere <cetinderedc@gmail.com>
2020-02-29 12:34:33 -0500

    update from Intellij

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,6 @@ package test;
 public class Login {
     public static void main(String[] args) {
         // some code
+        System.out.println("it is from Intellij for Github");
     }
 }
--- src/test/ReserveRoom.java
+++ src/test/ReserveRoom.java
@@ -2,7 +2,6 @@ package test;
 
 public class ReserveRoom {
     // Hi guys how are you
-
     // only team lead can do reserve
     // Bye Bye
 }
